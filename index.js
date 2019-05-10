0512280703cfb7151d9865125158d81ee2874962
Lance Russell <lancebryanrussell@gmail.com>
2019-05-09 22:25:13 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//Click a start button to start the quiz
+//Not be able to skip questions
+//Answer a series of 5 or more multiple choice questions
+//See which question they're on (e.g., "7 out of 10")
+//See their current score (e.g., "5 correct, 2 incorrect")
+//Receive feedback and, if incorrect, see the correct answer.
+//Move to the next question by interactive element or automatically
+//See overall score at the end of the quiz
+//Be able to start a new quiz
+
+const questions = [
+    {question:
+    },{
+
+    },]
